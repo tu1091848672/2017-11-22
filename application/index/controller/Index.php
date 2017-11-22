183caa4a39480208a9dda43b009cd7bf59540793
tu1091848672 <asdzxctu@qq.com>
2017-11-22 14:26:53 +0800

    1
    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,5 +1,6 @@
 <?php
 namespace app\index\controller;
+use think
 
 class Index
 {
---
+++ application/index/model/UserModel.php
@@ -0,0 +1,9 @@
+<?php
+namespace application\index\model;
+
+use think\Model;
+
+class UserModel extends Model
+{
+
+}
